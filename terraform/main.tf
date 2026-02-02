4b32316b328b16b676733d1601b43fcc9765aa07
DrLemesh <alonlemesh@gmail.com>
2026-02-02 22:45:07 +0200

    tf vpc,igw,rt,ecr with frontend and backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,8 @@
- resource "aws_vpc" "infrascore_vpc" {
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "infrascore_vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
@@ -14,5 +18,52 @@ resource "aws_subnet" "infrascore_subnet" {
 
   tags = {
     Name = "infrascore-subnet"
+  }
+}
+resource "aws_internet_gateway" "infrascore_igw" {
+  vpc_id = aws_vpc.infrascore_vpc.id
+
+  tags = {
+    Name = "infrascore-igw"
+  }
+}
+resource "aws_route_table" "infrascore_rt" {
+  vpc_id = aws_vpc.infrascore_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.infrascore_igw.id
+  }
+
+  tags = {
+    Name = "infrascore-rt"
+  }
+}
+
+# קישור הטבלה לסאבנט שיצרת קודם
+resource "aws_route_table_association" "infrascore_rta" {
+  subnet_id      = aws_subnet.infrascore_subnet.id
+  route_table_id = aws_route_table.infrascore_rt.id
+}
+
+# ECR Repositories
+
+# Backend Repository
+resource "aws_ecr_repository" "infrascore_backend" {
+  name                 = "infrascore-backend"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+# Frontend Repository
+resource "aws_ecr_repository" "infrascore_frontend" {
+  name                 = "infrascore-frontend"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
   }
 }
